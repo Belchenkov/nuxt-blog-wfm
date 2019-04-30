eda6810e0b736849b613ef1cec4043a5b8bc9391
Aleksey Belchenkov <u608110@gmail.com>
2019-04-30 05:57:21 +0300

    Component Post

--- components/main/Post.vue
+++ components/main/Post.vue
@@ -1,20 +1,65 @@
 <template>
-    <article class="post">
-
-    </article>
+  <el-card
+    shadow="hover"
+    :body-style="{padding: 0}"
+    class="post"
+  >
+    <header slot="header" class="post-header">
+      <h2>Post Title</h2>
+      <small>
+        <i class="el-icon-time"></i>
+        {{ new Date().toLocaleString() }}
+      </small>
+    </header>
+    <div class="post-body">
+      <img
+        src="https://www.hostinger.ru/rukovodstva/wp-content/uploads/sites/8/2018/08/chto-takoe-javascript.jpg"
+        alt="js"
+        class="post-image"
+      >
+    </div>
+    <footer class="post-footer">
+      <el-button round @click="openPost">Открыть</el-button>
+      <span>
+        <i class="el-icon-message"></i> 12
+      </span>
+    </footer>
+  </el-card>
 </template>
 
 <script>
-
     export default {
-
+      methods: {
+        openPost () {
+          console.log('open');
+        }
+      }
     }
 </script>
 
 <style lang="scss" scoped>
   .post {
-    height: 200px;
-    border: 1px solid black;
-    margin-bottom: 2rem;
+    margin-bottom: 1.5rem;
+  }
+
+  .post-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .post-img {
+    width: 100%;
+    height: auto;
+  }
+
+  .post-body {
+  }
+
+  .post-footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
   }
 </style>
