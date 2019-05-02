123e6e17956be8e25dcfb3ee9f17161e7e7afdc3
Aleksey Belchenkov <u608110@gmail.com>
2019-05-02 11:18:56 +0300

    Create Stucture Pages

--- components/main/Navigation.vue
+++ components/main/Navigation.vue
@@ -1,6 +1,7 @@
 <template>
     <nav>
-      <h1>SSR Blog</h1>
+      <img src="https://img.icons8.com/dusk/34/000000/rss.png" />
+      <h1 class="logo">Nuxt Blog</h1>
     </nav>
 </template>
 
@@ -11,13 +12,22 @@
 </script>
 
 <style lang="scss" scoped>
+  @import url('https://fonts.googleapis.com/css?family=Wendy+One');
+
+  .logo {
+    margin-left: 10px;
+    font-family: 'Wendy One', sans-serif;
+    text-shadow: 0 0 0 6px #000;
+    font-size: 1.5rem;
+  }
+
   nav {
     display: flex;
     align-items: center;
     justify-content: center;
     height: 100%;
     border-bottom: 1px solid #e6e6e6;
-    background: #34a041;
+    background: #2967a0;
     color: #fff;
     text-shadow: 0 0 4px #000;
   }
---
+++ layouts/admin.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="admin-layout-wrap">
+    <el-container class="h-100">
+      <el-aside width="200px">
+
+      </el-aside>
+      <el-main>
+        <nuxt />
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss" scoped>
+  .admin-layout-wrap {
+    width: 100%;
+    height: 100vh;
+  }
+
+</style>
