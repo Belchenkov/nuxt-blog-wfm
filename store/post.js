8f449e129310eecde1b6f9afb1c5b095503aaaed
Aleksey Belchenkov <u608110@gmail.com>
2019-05-06 06:53:56 +0300

    Page Update  Post

--- store/post.js
+++ store/post.js
@@ -1,14 +1,26 @@
+const posts = [
+  {title: 'Post 1', date: new Date(), views: 22, comments: [1, 2], _id: 'id1'},
+  {title: 'Post 2', date: new Date(), views: 22, comments: [1, 2], _id: 'id2'},
+  {title: 'Post 3', date: new Date(), views: 22, comments: [1, 2], _id: 'id3'}
+];
+
 export const actions = {
   async fetchAdmin({}) {
     return await new Promise(resolve => {
       setTimeout(() => {
-        resolve([
-          {title: 'Post 1', date: new Date(), views: 22, comments: [1, 2], _id: Math.random()},
-          {title: 'Post 2', date: new Date(), views: 22, comments: [1, 2], _id: Math.random()},
-          {title: 'Post 3', date: new Date(), views: 22, comments: [1, 2], _id: Math.random()}
-        ])
-      }, 2000)
+        resolve(posts)
+      }, 1000)
     });
+  },
+  async fetchAdminById({}, id) {
+    return await new Promise(resolve => {
+      setTimeout(() => {
+        resolve(posts.find(p => p._id === id));
+      }, 1000)
+    });
+  },
+  async update({}, {id, text}) {
+
   },
   async remove({}, id) {
 
