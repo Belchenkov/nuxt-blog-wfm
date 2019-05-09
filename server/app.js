6a735af39cfd180b8a31ea84e513c69ea7c046d7
Aleksey Belchenkov <u608110@gmail.com>
2019-05-09 21:30:43 +0300

    Config Files

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const keys = require('./keys');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
