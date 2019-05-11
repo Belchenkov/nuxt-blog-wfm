f38800a8b05f35cad6031bf917464b0f96ea4e71
Aleksey Belchenkov <u608110@gmail.com>
2019-05-11 20:19:56 +0300

    Controllers, Routes, Models

--- server/app.js
+++ server/app.js
@@ -2,17 +2,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const keys = require('./keys');
+// Routes
+const authRoutes = require('./routes/auth.routes');
 
+const keys = require('./keys');
 const app = express();
 
+// Connect To Mongo
 mongoose.connect(keys.MONGO_URI, { useNewUrlParser: true })
   .then(() => {
     console.log('MongoDB Connected ...');
   })
   .catch(e => console.error(e));
 
+// Body Parser
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
 
 module.exports = app;
