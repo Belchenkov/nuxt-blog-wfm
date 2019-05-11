ed5546b111c0c02ff1b83c5822165d0c13f89fb7
Aleksey Belchenkov <u608110@gmail.com>
2019-05-11 20:07:04 +0300

    Mongo Connected

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const keys = require('./keys');
 
 const app = express();
+
+mongoose.connect(keys.MONGO_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log('MongoDB Connected ...');
+  })
+  .catch(e => console.error(e));
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
--- server/keys/keys.dev.js
+++ server/keys/keys.dev.js
@@ -1,3 +1,3 @@
 module.exports = {
-
+  MONGO_URI: 'mongodb://belchenkov:12qwasZX@ds155396.mlab.com:55396/nuxt-blog-wfm'
 };
