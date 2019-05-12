15d22394d79c12f177f86b59f4e44ca6c82e748b
Aleksey Belchenkov <u608110@gmail.com>
2019-05-12 08:32:55 +0300

    Login

--- server/keys/keys.dev.js
+++ server/keys/keys.dev.js
@@ -1,3 +1,4 @@
 module.exports = {
-  MONGO_URI: 'mongodb://belchenkov:12qwasZX@ds155396.mlab.com:55396/nuxt-blog-wfm'
+  MONGO_URI: 'mongodb://belchenkov:12qwasZX@ds155396.mlab.com:55396/nuxt-blog-wfm',
+  JWT: 'nuxt-blog-jwt'
 };
